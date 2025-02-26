16d6b0f9f453f883c0ef8c07503ea77ebe7fac54
diveshbaviskar <diveshbaviskar19@gmail.com>
2025-02-26 12:56:27 +0530

    Add files via upload

---
+++ ServletDemo/src/main/java/com/servletexam/Student.java
@@ -0,0 +1,18 @@
+package com.servletexam;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class Student extends HttpServlet{
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	
+		System.out.println("Hello moto");
+	}	
+
+}
